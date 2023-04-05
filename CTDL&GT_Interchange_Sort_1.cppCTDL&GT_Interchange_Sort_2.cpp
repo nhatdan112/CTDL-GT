9f02f50e277f97976cfd6e5181df766fc6d917ac
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 15:52:28 +0700

    Create CTDL&GT_Interchange_Sort_1.cppCTDL&GT_Interchange_Sort_2.cpp

---
+++ CTDL&GT_Interchange_Sort_1.cppCTDL&GT_Interchange_Sort_2.cpp
@@ -0,0 +1,11 @@
+Nguồn tham khảo :https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-sap-xep-doi-cho-truc-tiep-interchange-sort
+void Interchange_Sort(int a[], int n){
+	int i,j;
+	for(i=0; i<n-1; i++){
+		for(j=i+1; j<n; j++){
+			if(a[i]>a[j]){
+				swap(a[i],a[j]);//hoán vị a[i] và a[j]
+			}
+		}
+	}
+}
