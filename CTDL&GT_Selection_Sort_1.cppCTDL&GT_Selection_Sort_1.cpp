76ff2079458eefa49ab29dab4d875d78d81c414d
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 16:02:24 +0700

    Create CTDL&GT_Selection_Sort_1.cppCTDL&GT_Selection_Sort_1.cpp

---
+++ CTDL&GT_Selection_Sort_1.cppCTDL&GT_Selection_Sort_1.cpp
@@ -0,0 +1,5 @@
+Ví dụ :cho danh sách gồm các con số :12,56,32,67,32,8
++Thuật toán hoạt động dựa trên việc chọn phần tử nhỏ nhất trong danh sách đổi chỗ lên vị trí đầu tiên trong danh sách :
++8 nhỏ nhất đưa 8 lên đầu : danh sách còn lại n-1 phần tử (loại phần tử nhỏ nhất vừa đưa lên đầu khỏi danh sách cần sắp xếp ),
+danh sách còn :12,56,32,67,32 vì 12 đã ở đầu danh sách nên danh sách còn :56,32,67 tiếp tục đưa 32 lên đầu rồi 56 rồi cuối cùng là 67 
+  ta có danh sách gồm các số được loại đi dần dần là :8,12,32,56,67
