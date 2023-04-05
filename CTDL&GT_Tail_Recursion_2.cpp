414a48f7f12b78d6f0e5881e31f23a0291fdae14
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 17:20:43 +0700

    Create CTDL&GT_Tail_Recursion_2.cpp

---
+++ CTDL&GT_Tail_Recursion_2.cpp
@@ -0,0 +1,14 @@
+Hàm đệ quy tìm ước chung lớn nhất 
+int UCLN(int m, int n){
+  int r;
+  if(m<n) return UCLN(n,m);
+  r = m % n;
+  if(r == 0) return n;
+  else return UCLN(n,r);
+}int UCLN(int m, int n){
+  int r;
+  if(m<n) return UCLN(n,m);
+  r = m % n;
+  if(r == 0) return n;
+  else return UCLN(n,r);
+}
