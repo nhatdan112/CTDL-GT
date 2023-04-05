522b3a1c6675a9445a83dd1ff66b2a8dbfeb0508
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 16:38:55 +0700

    Create CTDL&GT_Quick_Sort_1.cpp

---
+++ CTDL&GT_Quick_Sort_1.cpp
@@ -0,0 +1,7 @@
+Ví dụ:cho dãy số :12,46,22,6,7,4,8
+  Chọn phần tử giữa làm phần tử trung gian sau đó chia ra làm 2 mảng :
+    12,46,22 ->6<-7,4,8
+      sau đó so sánh từng phần tử trong mảng với phần tử trung gian 
+      -bên phải là những phần tử >phần tử trung gian 
+      -Bên trái là những phần tủ <phần tử trung gian 
+      -ta có mảng sau khi sắp xếp là :4,6,7,8,12,22,46
