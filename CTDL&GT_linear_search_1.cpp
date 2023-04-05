ca3e6f71097aa6c9faa0a26854e9c33f144bfcfe
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 15:40:07 +0700

    Create CTDL&GT_linear_search_1.cpp

---
+++ CTDL&GT_linear_search_1.cpp
@@ -0,0 +1,7 @@
+Ví dụ: cho danh sách các loại thước gồm các chiều cao khác nhau (đơn vị :cm):12,23,20,56,43,67,89
+  Đề tìm vị cây thước có độ dài 56cm thì thuật toán sẽ thực hiện như sau:
++B1:lấy phần tử x so sánh với phần tử đầu tiên 
++B2:Phần tử đầu tiên phần tử cần tìm ->tiếp tục lấy phần tử x so sánh với phần tử thứ 2
++B3 :Phần tử thứ 2 <phần tử cần tìm ->tiếp tục lấy phần tử x so sánh với phần tử thứ 3
++B4:Phần tử thứ 3 <phần tử cần tìm ->tiếp tục lấy phần tử x so sánh với phần tử thứ 4
++B5 Phần tử thứ 4 =phần tử cần tìm ->xuất phần tử thứ 4 và kết thúc thuật toán 
