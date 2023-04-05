fe3d803535d31818dcb572dee097a6e5667f4773
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 16:18:58 +0700

    Create CTDL&GT_Insertion_Sort_1.cpp

---
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -0,0 +1,9 @@
+Ví dụ :cho dãy số :53,24,96,245,73
+thuật toán sắp xếp chèn trực tiếp hoạt động bằng cách :chèn từng phần tử vào dãy sao cho vị trí để chèn phần tử đảm bảo phần tử đó phải lớn hơn phần tử phía bên trái 
+và nhỏ hơn phần tử phía bên phải :
++chèn 24 lên đầu
++chèn 54 vào giữa 24 và 73
++Chèn 73 vào giữa 54 và 96
++Chèn 96 vào giữa 73 và 245 ta có :
+  24,54,73,96,245 là dãy đã được sắp xếp 
+  
