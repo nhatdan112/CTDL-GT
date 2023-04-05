9c6a45fcfcd13e8af3294bb5d82fecbf90bc5076
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 17:05:56 +0700

    Update CTDL&GT_Stack_2.cpp

--- CTDL&GT_Stack_2.cpp
+++ CTDL&GT_Stack_2.cpp
@@ -1,6 +1,47 @@
-#define MAX 100
-struct stack{
-    int top;
-    int A[MAX];
-};
-typedef struct stack STACK;
+#define max 10000
+int Stack[max];
+int Top;
+
+void StackInit(){
+	Top = -1;//khởi tạo giá trị phần tử đầu danh sách 
+}
+
+void push(int V){//kiểm tra stack có đầy hay không 
+	if(Top > max-1){
+		cout<<"Stack is full";
+	}else{
+		Top++;
+		Stack[Top] = V;
+	}
+}
+
+int pop(){
+	if (Top == -1){
+		cout<<"Stack is empty";//kiểm tra stack có rỗng hay không 
+		return -1;
+	}else{
+		int res = Stack[Top];
+		Top--;
+		return res;
+	}
+}
+
+int empty(){
+	if(Top == -1){
+		return 0;//stack rỗng
+	}
+	return 1;//stack không rỗng 
+}
+
+int size(){
+	return Top+1;//kích cỡ stack 
+}
+int top(){
+	if (Top == -1){
+		cout<<"Stack is empty";
+		return -1;
+	}else{
+		int res = Stack[Top];
+		return res;
+	}
+// trả về giá trị đầu stack 
