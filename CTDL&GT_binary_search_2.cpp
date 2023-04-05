d356e3f012d0ce84ef506d81c8cf4a2bbb7ee184
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2023-04-05 15:22:27 +0700

    Create CTDL&GT_binary_search_2.cpp

---
+++ CTDL&GT_binary_search_2.cpp
@@ -0,0 +1,6 @@
+Ví dụ:Cho danh sách gồm các đồ vật :bút chì,bút máy ,thước,tẩy,máy tính
+Để tìm bút máy trong danh sách trên thì :
++b1:bút chì bắt đầu từ vị trí 0 đến máy tính là 4
++B2:lấy vị trí ở giữa là 6/3=2:thước
++B3:so sánh vị trí thước với các vị trí khác ta có (bút máy ở vị trí thứ 1) 2>1 ,2<3,=>vị trí cần tìm nằm ở bên trái của danh sách 
++B4:giảm vị trí giữa đi 1 và bắt đầu so sánh tiếp vị trí đã giảm ta có 1=1: giá trị vị trí bằng nhau ,đây là vị trí chứa bút máy . 
